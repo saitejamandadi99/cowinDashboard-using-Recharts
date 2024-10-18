19aa71cff4070a49ae3a11e558266aa23a19c34c
saitejamandadi99 <saitejamandadi99>
2024-10-18 05:12:04 +0000

    added logo and website heading

--- src/components/CowinDashboard/index.js
+++ src/components/CowinDashboard/index.js
@@ -1,1 +1,24 @@
 // Write your code here
+import {Component} from 'react'
+
+import './index.css'
+
+class CowinDashboard extends Component {
+  render() {
+    return (
+      <div className="cowinDashboardContainer">
+        <div className="logoContainer">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/cowin-logo.png"
+            alt="website logo"
+            className="websiteLogo"
+          />
+          <h1 className="logoHeading">Co-WIN</h1>
+        </div>
+        <h1 className="websiteHeading">CoWIN Vaccination in India</h1>
+      </div>
+    )
+  }
+}
+
+export default CowinDashboard
